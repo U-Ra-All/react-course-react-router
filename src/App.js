813d9cf3fa943ea32ead1d953f0548e87c34d62d
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-11-07 12:16:13 +0200

    05-Работа со Ссылками

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <Route path='/home'>
-        <Home />
-      </Route>
-      <Route path='/about'>
-        <About />
-      </Route>
+      <Header />
+      <main>
+        <Route path='/home'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+      </main>
     </div>
   );
 }
